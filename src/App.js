056f5a01a1b9b77462e34332845d4284a7c78896
Ashadozzaman <shovoua@gmail.com>
2024-06-26 12:39:08 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello, from cloudOps project !
+        </p>
+        <p>
+         release 2.0
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/mohammad-akif2000/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More about Me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
